435c51c820da76d6e83c47670ee569cea314406b
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-08-17 13:42:12 +0200

    Adds seed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from generator import Grid, Instance
+import numpy as np
 from pprint import pprint
 
+np.random.seed(11)
 instance = Instance(10, 8, conglomeration_ratio=0.4, obstacle_ratio=0.3)
 instance.plot()
+print(instance.adj)
 # pprint(instance.grid.to_adj())
