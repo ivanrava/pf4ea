62b644cbd8074aad1f87fcca107b41cf57ffb62d
Ivan Ravasi <ivan4.ravasi@gmail.com>
2023-08-24 22:21:20 +0200

    Better plot styles

--- eda.py
+++ eda.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import FuncFormatter
 import seaborn as sns
 
-
 if __name__ == '__main__':
     # Read data
     df = pd.read_csv('42.csv', sep=';')
@@ -27,63 +26,60 @@ if __name__ == '__main__':
     df['free_cells'] = df['size'] - df['obstacles']
 
 
-    def condition_func(row):
+    def aspect_ratio(row):
         if row['width'] >= row['height']:
-            return np.round(row['height']/row['width'],decimals=2)
+            return np.round(row['height'] / row['width'], decimals=2)
         else:
-            return np.round(row['width']/row['height'],decimals=2)
+            return np.round(row['width'] / row['height'], decimals=2)
 
 
-    df['aspect_ratio'] = df.apply(condition_func, axis=1)
-
-    print(df)
-
-
-
-
-    print(df)
-
-
-
-
-
+    df['aspect_ratio'] = df.apply(aspect_ratio, axis=1)
 
     with pd.option_context('display.max_columns', 40):
         print(df.describe(include='all'))
 
-    df_memory_groups = df.groupby('size').agg(MemoryMin=('memory', 'min'),
-                           MemoryMean=('memory', 'mean'),
-                           MemoryMax=('memory', 'max'))
+    df_no_timeout = df.loc[df['status'] != 'timeout']
 
-    sns.lineplot(data=df_memory_groups)
-    plt.title('size - memory line ')
+    sns.set_theme()
+    # Lineplot - memory against size
+    plt.figure(figsize=(8, 6))
+    df_memory_groups = df.groupby('size').agg(Best=('memory', 'min'),
+                                              Mean=('memory', 'mean'),
+                                              Worst=('memory', 'max'))
+
+    sns.lineplot(data=df_memory_groups, markers=True, dashes=False, markersize=8)
+    plt.title('Memory consumption w.r.t. grid size', fontsize=18)
+    plt.xlabel('Size')
+    plt.ylabel('Memory [MB]')
     plt.show()
 
-
-
-    plt.figure(figsize=(16,8))
-    sns.violinplot(x='size', y='memory', data=df, linewidth=0)
-    plt.title('size - memory violin ')
+    # Violinplot - memory against size
+    plt.figure(figsize=(16, 8))
+    ax = sns.violinplot(x='size', y='memory', data=df, linewidth=0)
+    plt.ylim((97, 110))
+    sns.despine(offset=10, trim=True)
+    plt.title('Memory consumption distribution w.r.t grid size', fontsize=22)
+    plt.xlabel('Size')
+    plt.ylabel('Memory [MB]')
+    plt.grid(zorder=0)
+    ax.set_axisbelow(True)
     plt.show()
 
-
-
-
-# graphic size - resolution time (filtered on status success)
+    # graphic size - resolution time (filtered on status success)
     df_success = df.loc[df['status'] == 'success']
 
     df_diagonal = df_success.loc[df['h'] == 'diagonal']
     df_dijkstra = df_success.loc[df['h'] == 'dijkstra']
 
-    #grafico size - process time (filtered on success )
-    sns.lineplot(data=df_diagonal.groupby('size').agg(ProcessTimeMeanDiagonal=('process_time','mean')),  palette=['red'])
-    sns.lineplot(data=df_dijkstra.groupby('size').agg(ProcessTimeMeanDijkstra=('process_time', 'mean')), palette=['blue'])
-    plt.title('size - process time by heuristic on success')
+    # grafico size - process time (filtered on success )
+    sns.lineplot(data=df, x='size', y='process_time', hue='h')
+    plt.legend(labels=['Dijkstra relaxed paths', '95% confidence', 'Diagonal distance', '95% confidence'])
+    plt.xlabel('Size')
+    plt.ylabel('Process time [s]')
+    plt.title('Process time w.r.t. heuristics')
     plt.show()
 
-
-
-#histogram count status per obstacle ratio
+    # histogram count status per obstacle ratio
     obstacle_ratio_groups = df.groupby('obstacle_ratio')['status'].value_counts()
     print(obstacle_ratio_groups)
 
@@ -92,99 +88,101 @@ if __name__ == '__main__':
 
     print(df_obstacle_ratio_groups)
 
-    df_obstacle_ratio_groups.plot(kind='bar')
-    plt.xlabel('obstaclo_ratio')
-    plt.ylabel('tot')
-    plt.title('Conteggio occorrenze status per obstacle ratio')
+    g = df_obstacle_ratio_groups.plot(kind='bar')
+    g.set_xticklabels(labels=['0%', '25%', '50%', '75%', '90%'], rotation=0)
+    plt.xticks(rotation=0)
+    plt.xlabel('Obstacles')
+    plt.ylabel('')
+    plt.title('Status count w.r.t. obstacle ratio')
     plt.legend(title='Status')
     plt.show()
 
-    print(df.info)
-
-#histogram grid gen time by size
-    df_grid_gen_time_group = df.groupby('size').agg(GridGenTimeMean=('grid_gen_time','mean'),
-                                                    AgentsGenTimeMean=('agents_gen_time','mean'),
-                                                    ResolutionTimeMean=('resolution_time','mean'))
-    print(df_grid_gen_time_group)
-    sns.lineplot(data=df_grid_gen_time_group)
-    plt.title('size - grid gen time')
+    # histogram grid gen time by size
+    df_grid_gen_time_group = df.groupby('size').agg(GridGenTimeMean=('grid_gen_time', 'mean'),
+                                                    AgentsGenTimeMean=('agents_gen_time', 'mean'),
+                                                    ResolutionTimeMean=('resolution_time', 'mean'))
+    sns.lineplot(data=df_grid_gen_time_group, errorbar=None, markers=True, dashes=False, markersize=6)
+    plt.legend(labels=['Grid generation', 'Agents generation', 'Resolution time'])
+    plt.title('Generation and solver elapsed times')
     plt.show()
 
-
-#random and optimal agents
+    # random and optimal agents
     df_agent_random = df.loc[df['agent_generator'] == 'random'].loc[df['status'] != 'timeout']
     df_agent_optimal = df.loc[df['agent_generator'] == 'optimal'].loc[df['status'] != 'timeout']
 
     df_agent_random_group = df_agent_random.groupby('size').agg(AgentsGenRandomTimeMean=('agents_gen_time', 'mean'))
     df_agent_optimal_group = df_agent_optimal.groupby('size').agg(AgentsGenOptimalTimeMean=('agents_gen_time', 'mean'))
 
-    sns.lineplot(data=df_agent_random_group, palette=['red'])
-    sns.lineplot(data=df_agent_optimal_group, palette=['blue'])
-    plt.title('time optimal and random agents generator')
+    sns.lineplot(data=df_no_timeout, x='size', y='agents_gen_time', hue='agent_generator',
+                 errorbar=None, markers=True, dashes=False, markersize=8)
+    plt.legend(labels=['Pseudo-random', 'Through ReachGoal'])
+    plt.title('Agent generators time comparison')
     plt.show()
 
-#num agents - time
-    df_num_agents_group = df.groupby('num_agents').agg(AgentsGenTime=('agents_gen_time','mean'),
-                                                       ResolutionTime=('resolution_time','mean'))
-    print(df_grid_gen_time_group)
-    g =  sns.lineplot(data=df_num_agents_group)
-    g.set(xticks = [0, 1, 2, 3])
-    plt.title('num agents - agents gen time')
+    # num agents - time
+    df_num_agents_group = df.groupby('num_agents').agg(AgentsGenTime=('agents_gen_time', 'mean'),
+                                                       ResolutionTime=('resolution_time', 'mean'))
+    g = sns.lineplot(data=df_num_agents_group, errorbar=None, markers=True, dashes=False, markersize=7)
+    plt.legend(labels=['Agent generation time', 'Resolution time'])
+    g.set(xticks=[0, 1, 2, 3])
+    plt.title('Times w.r.t. # of agents')
+    plt.xlabel('# of agents')
+    plt.ylabel('[s]')
     plt.show()
 
-#filter agents on random and optimal
-    df_num_agents_group_random = df_agent_random.groupby('num_agents').agg(AgentsGenTimeRandom=('agents_gen_time','mean'),
-                                                                    ResolutionTimeRandom=('resolution_time','mean'))
-    df_num_agents_group_optimal = df_agent_optimal.groupby('num_agents').agg(AgentsGenTimeOptimal=('agents_gen_time', 'mean'),
-                                                                           ResolutionTimeOptimal=('resolution_time', 'mean'))
+    # filter agents on random and optimal
+    df_num_agents_group_random = df_agent_random.groupby('num_agents').agg(
+        AgentsGenTimeRandom=('agents_gen_time', 'mean'),
+        ResolutionTimeRandom=('resolution_time', 'mean'))
+    df_num_agents_group_optimal = df_agent_optimal.groupby('num_agents').agg(
+        AgentsGenTimeOptimal=('agents_gen_time', 'mean'),
+        ResolutionTimeOptimal=('resolution_time', 'mean'))
 
     sns.lineplot(data=df_num_agents_group_random, palette=['red', 'blue'])
     sns.lineplot(data=df_num_agents_group_optimal, palette=['green', 'black'])
-    plt.title('num agents - agents gen time')
+    plt.title('# of agents & times')
+    plt.xlabel('Times against agent generation criterias')
+    plt.ylabel('[s]')
     plt.show()
 
-    print(df.keys())
+    # heatmap memory occupation - obstacle
+    grid = np.zeros((5, 5))
+    for i, o in enumerate([0, 0.25, 0.5, 0.75, 0.9]):
+        for j, c in enumerate([0, 0.25, 0.5, 0.75, 1]):
+            dfoc = df.loc[df['obstacle_ratio'] == o].loc[df['conglomeration_ratio'] == c].loc[df['status'] == 'success']
+            grid[i, j] = np.mean(dfoc['memory'])
 
+    grid[0, 1] = grid[0, 2] = grid[0, 3] = grid[0, 4] = grid[0, 0]
 
-
-# heatmap memory occupation - obstacle
-    grid = np.zeros((5,5))
-    for i,o in enumerate([0, 0.25, 0.5, 0.75, 0.9]):
-         for j,c in enumerate([0, 0.25, 0.5, 0.75, 1]):
-              dfoc = df.loc[df['obstacle_ratio'] == o].loc[df['conglomeration_ratio'] == c].loc[df['status'] == 'success']
-              grid[i,j] =  np.mean(dfoc['memory'])
-
-    grid[0,1] = grid[0,2] = grid[0,3] = grid[0,4] = grid[0,0]
-
-# Create a heatmap
+    # Create a heatmap
     sns.set()
-    a = sns.heatmap(grid, annot=True, xticklabels=['0%', '25%', '50%', '75%','100%'], yticklabels=['0%', '25%', '50%', '75%','90%'],
+    a = sns.heatmap(grid, annot=True, xticklabels=['0%', '25%', '50%', '75%', '100%'],
+                    yticklabels=['0%', '25%', '50%', '75%', '90%'],
                     fmt=".1f", cmap=sns.cm.rocket_r)
     a.invert_yaxis()
 
-# Add title
+    # Add title
     plt.xlabel('conglomeration_ratio')
     plt.ylabel('obstacle_ratio')
     plt.title("Heatmap memory occupation - obstacle")
     plt.show()
 
-
-#open closed states
-    sns.lineplot(data=df_diagonal.groupby('size').agg(InsertedStates=('inserted_states','mean')),  palette=['red'])
+    # open closed states
+    sns.lineplot(data=df_diagonal.groupby('size').agg(InsertedStates=('inserted_states', 'mean')), palette=['red'])
     sns.lineplot(data=df_dijkstra.groupby('size').agg(ClosedStates=('closed_states', 'mean')), palette=['blue'])
     plt.yscale('log')
     plt.title('size - states inserted/closed')
     plt.show()
 
-#aspect ratio plot
+    # aspect ratio plot
 
     df_without_timeout = df.loc[df['status'] != 'timeout']
     process_time_aspect_ratio_groups = df_without_timeout.groupby('aspect_ratio')['status'].value_counts()
     df_process_time_aspect_ratio_groups = process_time_aspect_ratio_groups.unstack(fill_value=0)
-    success = df_process_time_aspect_ratio_groups['success']/(
-            df_process_time_aspect_ratio_groups['success']+df_process_time_aspect_ratio_groups['failure'])
+    success = df_process_time_aspect_ratio_groups['success'] / (
+            df_process_time_aspect_ratio_groups['success'] + df_process_time_aspect_ratio_groups['failure'])
     failure = df_process_time_aspect_ratio_groups['failure'] / (
-                df_process_time_aspect_ratio_groups['success'] + df_process_time_aspect_ratio_groups['failure'])
+            df_process_time_aspect_ratio_groups['success'] + df_process_time_aspect_ratio_groups['failure'])
     df_process_time_aspect_ratio_groups['success'] = success
     df_process_time_aspect_ratio_groups['failure'] = failure
 
@@ -195,22 +193,25 @@ if __name__ == '__main__':
     plt.legend(title='Status')
     plt.show()
 
-#plot line
-    sns.lineplot(data=df.groupby('aspect_ratio').agg(InsertedStates=('inserted_states','mean')),  palette=['red'])
+    # plot line
+    sns.lineplot(data=df.groupby('aspect_ratio').agg(InsertedStates=('inserted_states', 'mean')), palette=['red'])
     sns.lineplot(data=df.groupby('aspect_ratio').agg(ClosedStates=('closed_states', 'mean')), palette=['blue'])
     plt.title('aspect ratio - states inserted/closed')
     plt.show()
 
-#dijkstra diagonale states
-    sns.lineplot(data=df_diagonal.groupby('size').agg(InsertedStatesDiagonal=('inserted_states', 'mean')), palette=['red'])
+    # dijkstra diagonale states
+    sns.lineplot(data=df_diagonal.groupby('size').agg(InsertedStatesDiagonal=('inserted_states', 'mean')),
+                 palette=['red'])
     sns.lineplot(data=df_diagonal.groupby('size').agg(ClosedStatesDiagonal=('closed_states', 'mean')), palette=['blue'])
-    sns.lineplot(data=df_dijkstra.groupby('size').agg(InsertedStatesDijkstra=('inserted_states', 'mean')), palette=['green'])
-    sns.lineplot(data=df_dijkstra.groupby('size').agg(ClosedStatesDijkstra=('closed_states', 'mean')), palette=['black'])
+    sns.lineplot(data=df_dijkstra.groupby('size').agg(InsertedStatesDijkstra=('inserted_states', 'mean')),
+                 palette=['green'])
+    sns.lineplot(data=df_dijkstra.groupby('size').agg(ClosedStatesDijkstra=('closed_states', 'mean')),
+                 palette=['black'])
     plt.yscale('log')
     plt.title('size - states inserted/closed Dijkstra Diagonal')
     plt.show()
 
-#mosse wait
+    # mosse wait
     sns.lineplot(data=df.groupby('size').agg(Waits=('waits', 'mean')))
     plt.title('size - waits')
     plt.show()
