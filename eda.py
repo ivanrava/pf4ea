abaea06ce8029e1f7611cb7ec35ccd66ae15822a
rizzo08-coder <m.rizzo009@studenti.unibs.it>
2023-08-24 08:44:35 +0200

    some graphics (lineplot size - process time diagonal and dijkstra) (bar size - status )

--- eda.py
+++ eda.py
@@ -35,3 +35,34 @@ if __name__ == '__main__':
     sns.lineplot(data=df_memory_groups)
     sns.violinplot(x='size', y='memory', data=df)
     plt.show()
+
+
+
+
+    # graphic size - resolution time (filtered on status success)
+    df_success = df.loc[df['status'] == 'success']
+
+    df_diagonal = df_success.loc[df['h'] == 'diagonal']
+    df_dijkstra = df_success.loc[df['h'] == 'dijkstra']
+
+    #grafico size - process time (filtered on success )
+    sns.lineplot(data=df_diagonal.groupby('size').agg(ProcessTimeMeanDiagonal=('process_time','mean')),  palette=['red'])
+    sns.lineplot(data=df_dijkstra.groupby('size').agg(ProcessTimeMeanDijkstra=('process_time', 'mean')), palette=['blue'])
+    plt.show()
+
+
+#histogram count status per obstacle ratio
+    obstacle_ratio_groups = df.groupby('obstacle_ratio')['status'].value_counts()
+    print(obstacle_ratio_groups)
+
+    # Convertire la Serie risultante in un DataFrame
+    df_obstacle_ratio_groups = obstacle_ratio_groups.unstack(fill_value=0)
+
+    print(df_obstacle_ratio_groups)
+
+    df_obstacle_ratio_groups.plot(kind='bar')
+    plt.xlabel('obstaclo_ratio')
+    plt.ylabel('tot')
+    plt.title('Conteggio occorrenze status per obstacle ratio')
+    plt.legend(title='Status')
+    plt.show()
